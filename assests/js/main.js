71de9abd32029b8eef4bc4328679c651db8310c6
sudhanshu0601 <jhariya.sudhanshu06@gmail.com>
2025-07-09 11:36:39 +0530

    completed faq section along with functionality

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,13 +1,48 @@
-const toggleButton = document.querySelector('.my__toggle');
-const navbar= document.querySelector('.my__navbar');
+/* navbar section*/
+const toggleButton = document.querySelector(".my__toggle");
+const navbar = document.querySelector(".my__navbar");
 
-toggleButton.addEventListener('click', () => {
-  navbar.classList.toggle('show');
-  toggleButton.classList.toggle('show');
-  if (navbar.classList.contains('show')) {
-    toggleButton.classList.replace('bi-list', 'bi-x');
+toggleButton.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+  toggleButton.classList.toggle("show");
+  if (navbar.classList.contains("show")) {
+    toggleButton.classList.replace("bi-list", "bi-x");
   } else {
-    toggleButton.classList.replace('bi-x', 'bi-list');
+    toggleButton.classList.replace("bi-x", "bi-list");
   }
-  
-})
+});
+// end of navbar section
+
+//portfolio section
+
+//end of porfolio section
+
+/* faq section*/
+const tabButtons = document.querySelectorAll(".faq__main__tabs .nav-link");
+const tabPanes = document.querySelectorAll(".faq__main__content .tab-pane");
+
+tabButtons.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    tabButtons.forEach((b) => b.classList.remove("active"));
+
+    btn.classList.add("active");
+
+    tabPanes.forEach((pane) => {
+      pane.classList.remove("show", "active");
+    });
+
+    tabPanes[index].classList.add("show", "active");
+  });
+});
+
+
+const faqItems = document.querySelectorAll(".faq__main__content__item");
+
+faqItems.forEach((item) => {
+  const heading = item.querySelector("h3");
+
+  heading.addEventListener("click", () => {
+    item.classList.toggle("faq-active");
+  });
+});
+//end of faq section
